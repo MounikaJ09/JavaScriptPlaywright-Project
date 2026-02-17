7902ce149702705398e044920e845aa6c414d316
MounikaJ09 <jaladimounika89@gmail.com>
2026-02-17 08:28:34 +0530

    Add Chapter_02_Java_Concepts/Hoisting.js

---
+++ Chapter_02_Java_Concepts/Hoisting.js
@@ -0,0 +1,11 @@
+// console.log(a);
+// var a=10;
+// console.log(a); // Output: 10
+
+/* console.log(b);
+let b=20;
+console.log(b); // Output: 20   */
+
+console.log(c);
+const c=30;
+console.log(c); // Output: 30   
